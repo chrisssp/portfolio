e8f31877f5592de1a26a0e587e105491bc837f63
chrisssp <christian.serrano.puertos@gmail.com>
2026-04-17 18:01:24 -0600

    chore: setup infrastructure and project dependencies

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 const locales = ["es", "en"];
-const defaultLocale = ["en"];
+const defaultLocale = "en";
 
 export function proxy(request: NextRequest) {
    const pathname = request.nextUrl.pathname;
